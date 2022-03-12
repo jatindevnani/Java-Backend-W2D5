19ec5ba3de391ee2d4f6055d84c1734213128cba
jatindevnani <81103012+jatindevnani@users.noreply.github.com>
2022-03-12 13:43:15 +0530

    Problem 3 completed

---
+++ src/com/masai/problem_3/RoadSideHotel.java
@@ -0,0 +1,11 @@
+package com.masai.problem_3;
+
+public class RoadSideHotel implements Hotel {
+    public void chickenBiryani() {
+        System.out.println("Biryani from Road Side Hotel");
+    }
+
+    public void masalaDosa() {
+        System.out.println("MasalaDosa from Road Side Hotel");
+    }
+}
---
+++ src/com/masai/problem_3/TajHotel.java
@@ -0,0 +1,16 @@
+package com.masai.problem_3;
+
+public class TajHotel implements Hotel {
+    public void chickenBiryani() {
+        System.out.println("Biryani from Taj Hotel");
+    }
+
+    public void masalaDosa() {
+        System.out.println("MasalaDosa from Taj Hotel");
+    }
+
+    //TajHotel's specific method
+    public void welcomeDrink(){
+        System.out.println("Welcome Drink from the TajHotel");
+    }
+}
